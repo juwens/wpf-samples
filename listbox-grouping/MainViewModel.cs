334e8a09d0511a2455988742733459bc58d27a6d
jjaehrig <jens-uwe.jaehrig@fsd-web.de>
2020-01-27 10:46:51 +0100

    initial commit

---
+++ listbox-grouping/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace listbox_grouping
+{
+    public class MainViewModel
+    {
+        public ObservableCollection<Planet> Planets { get; } = new ObservableCollection<Planet>();
+
+        public MainViewModel()
+        {
+            new List<Planet>()
+            {
+                new Planet{Name="Mercury", IsInner=true},
+                new Planet{Name="Venus", IsInner=true},
+                new Planet{Name="Earth", IsInner=true},
+                new Planet{Name="Mars", IsInner=true},
+                new Planet{Name="Jupiter",IsInner=false},
+                new Planet{Name="Satur", IsInner= false},
+                new Planet{Name="Uranus", IsInner=false},
+                new Planet{Name="Neptune", IsInner=false},
+            }.ForEach(Planets.Add);
+        }
+    }
+}
---
+++ listbox-grouping/Planet.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace listbox_grouping
+{
+    public class Planet
+    {
+        public string Name { get; set; }
+        public bool IsInner { get; set; }
+    }
+}
